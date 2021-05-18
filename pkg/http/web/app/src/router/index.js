0ceaea90a164d701fd0d9dd11341d7fc1e5e1811
Robin Huiser <robin@rdc.pt>
2021-05-18 22:20:13 +0100

    Initial working app + (node) server

--- pkg/http/web/app/src/router/index.js
+++ pkg/http/web/app/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Profile from "../views/Profile.vue";
+import ExternalApiView from "../views/ExternalApi.vue";
+
+import { authGuard } from "../auth/authGuard";
 
 Vue.use(VueRouter)
 
@@ -17,13 +21,25 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    component: Profile,
+    beforeEnter: authGuard
+  },
+  {
+    path: "/external-api",
+    name: "external-api",
+    component: ExternalApiView,
+    beforeEnter: authGuard
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
 export default router
